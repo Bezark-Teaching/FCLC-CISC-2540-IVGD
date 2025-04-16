a8fd60a747842412a0b2f14ed42e7d052de4cac9
bezark <johnbezark@gmail.com>
2025-04-16 13:32:15 -0400

    post class

---
+++ Sessions/4-15/dialoguedemo/npc.gd
@@ -0,0 +1,28 @@
+extends Sprite2D
+
+@export var dialogue : Dialogue
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func  _process(delta):
+	if Input.is_action_just_pressed("ui_cancel"):
+		dialogue.normals_dialgoue = dialogue.quest_complete_dialogue
+		current_line = 0
+	if Input.is_action_just_pressed("ui_accept") and $DialogueBox.visible:
+		current_line += 1
+		if current_line < dialogue.normals_dialgoue.size():
+			$DialogueBox/Label.text = dialogue.normals_dialgoue[current_line]
+	
+var current_line = 0
+
+func _on_area_2d_body_entered(body):
+	$DialogueBox.show()
+	$DialogueBox/Label.text = dialogue.normals_dialgoue[current_line]
+
+
+
+
+func _on_area_2d_body_exited(body):
+	$DialogueBox.hide()
