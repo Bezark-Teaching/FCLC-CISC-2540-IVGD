1e41ed0672f105c18cb97c297fb64e0f020f8235
bezark <johnbezark@gmail.com>
2025-04-29 11:30:31 -0400

    post 4-15

--- Sessions/4-15/dialoguedemo/dialogue.gd
+++ Sessions/4-15/dialoguedemo/dialogue.gd
@@ -1,5 +1,7 @@
 extends Resource
 class_name Dialogue
 
-@export var normals_dialgoue : Array[String]
+@export var normal_dialgoue : Array[String]
 @export var quest_complete_dialogue : Array[String]
+
+@export var fancy_dialogue : Array[LineOfDialogue]
