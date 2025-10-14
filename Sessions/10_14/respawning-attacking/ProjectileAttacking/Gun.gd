35a76381790949e432200310c380012355ac53bb
bezark <john@bezark.fun>
2025-10-14 16:19:12 -0400

    pre 10_14

--- Sessions/10_14/respawning-attacking/ProjectileAttacking/Gun.gd
+++ Sessions/10_14/respawning-attacking/ProjectileAttacking/Gun.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-const PROJECTILE = preload("res://ProjectileAttacking/Projectile.tscn")
+const TSHIRT = preload("res://ProjectileAttacking/Projectile.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -12,7 +12,7 @@ func _process(delta):
 
 
 func _on_timer_timeout():
-	print("Bang!")
-	var new_bullet = PROJECTILE.instantiate()
-	new_bullet.position = $Marker.position
-	add_child(new_bullet)
+	print("Free shirt!")
+	var tshirt = TSHIRT.instantiate()
+	tshirt.position = $Marker.position
+	add_child(tshirt)
