55821463560ec06949dbdfd4f5edf39d1919dc0a
bezark <john@bezark.fun>
2025-10-17 14:17:09 -0400

    post 10-17

---
+++ Sessions/10_17/changingscenesexample/top_down_character.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+@export var speed = 10
+
+func _process(delta):
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= speed
+	elif Input.is_action_pressed("ui_down"):
+		velocity.y += speed
+	elif Input.is_action_pressed("ui_left"):
+		velocity.x -= speed
+	elif Input.is_action_pressed("ui_right"):
+		velocity.x += speed
+	else:
+		velocity = Vector2(0,0)
+		pass
+	move_and_slide()
+	if Input.is_action_just_pressed("ui_select"):
+		Stats.score += 1
+		print(Stats.score)
