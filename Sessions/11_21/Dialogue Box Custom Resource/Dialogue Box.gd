b93e14287b5925215d831e9fd73a047252e57e45
bezark <john@bezark.fun>
2025-11-21 14:10:22 -0500

    post class

--- Sessions/11_21/Dialogue Box Custom Resource/Dialogue Box.gd
+++ Sessions/11_21/Dialogue Box Custom Resource/Dialogue Box.gd
@@ -1,6 +1,6 @@
 extends Panel
 
-@export var lines : Dialogue
+@export var lines : Array[LineOfDialogue]
 var current_line = -1
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,8 +11,8 @@ func _ready():
 func _process(delta):
 	if Input.is_action_just_pressed("ui_accept"):
 		current_line += 1
-		if lines.lines_of_dialogue.size()>current_line:
-			display(lines.lines_of_dialogue[current_line])
+		if lines.size()>current_line:
+			display(lines[current_line])
 
 
 @onready var portrait = $Portrait
